8d4ee91f113c7a19b39d275653e764776fe284f9
wenjun.swj <wenjun.swj@alibaba-inc.com>
2021-07-09 13:22:55 +0800

    BUG: Sampling over selected groupbys does not reflect the selection

--- pandas/tests/frame/methods/test_shift.py
+++ pandas/tests/frame/methods/test_shift.py
@@ -183,6 +183,30 @@ class TestDataFrameShift:
 
         tm.assert_frame_equal(result, expected)
 
+        # GH#42401
+        df3 = pd.concat([df1.iloc[:4, 1:3], df2.iloc[:4, :]], axis=1)
+        result = df3.shift(2, axis=1, fill_value=np.int_(0))
+        if not using_array_manager:
+            assert len(df3._mgr.blocks) == 2
+
+        expected = df3.take([-1, -1, 0, 1], axis=1)
+        expected.iloc[:, :2] = np.int_(0)
+        expected.columns = df3.columns
+
+        tm.assert_frame_equal(result, expected)
+
+        # Case with periods < 0
+        df3 = pd.concat([df1.iloc[:4, 1:3], df2.iloc[:4, :]], axis=1)
+        result = df3.shift(-2, axis=1, fill_value=np.int_(0))
+        if not using_array_manager:
+            assert len(df3._mgr.blocks) == 2
+
+        expected = df3.take([2, 3, -1, -1], axis=1)
+        expected.iloc[:, -2:] = np.int_(0)
+        expected.columns = df3.columns
+
+        tm.assert_frame_equal(result, expected)
+
     @pytest.mark.filterwarnings("ignore:tshift is deprecated:FutureWarning")
     def test_tshift(self, datetime_frame):
         # TODO: remove this test when tshift deprecation is enforced
